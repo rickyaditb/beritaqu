f2cc08deccfd1e66773ceba2e74ca61b3ee719a7
rickyaditb <rickyaditb@gmail.com>
2024-02-05 17:18:21 +0700

    feat: capitalize the first letter of source badge

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -6,7 +6,9 @@ export default function Card({ title, time, category, source, image}: NewsItem) 
   return (
     <article className="effect">
       <div className="group bg-cover bg-center bg-no-repeat relative rounded-t-lg p-5" style={{ backgroundImage: `linear-gradient(to top, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0) 25%), url('${image}')`, height: "250px" }}>
-        <button className="group-hover:opacity-0 transition ease-in-out duration-300 bg-red-400 px-4 py-1 font-bold text-white rounded text-2xl">{source}</button>
+        <button className="group-hover:opacity-0 transition ease-in-out duration-300 bg-red-400 px-4 py-1 font-bold text-white rounded text-2xl">
+          {source.charAt(0).toUpperCase() + source.slice(1)}
+        </button>
       </div>
       <div className="p-5 text-secondary">
         <p className="font-bold text-xl truncate-2-lines">{title}</p>
