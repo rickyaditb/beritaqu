407bb68c92f5f3ba6aa2aca0373062909ddc7619
rickyaditb <rickyaditb@gmail.com>
2024-02-07 14:57:45 +0700

    fix: add missing link navigation to article

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Sidebar from "./components/Sidebar";
 import Card from "./components/Card";
 import { getNews } from "../utils/database";
 import { NewsItem } from "@/utils/types";
+import Link from "next/link";
 
 export default async function Home() {
   const news = await getNews();
@@ -14,16 +15,17 @@ export default async function Home() {
         <main className="col-span-6 lg:col-span-5 lg:pl-5">
           <div className="grid md:grid-cols-2 gap-5">
             {news.slice(0, 10).map((item: NewsItem) => (
-              <Card
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                time={item.time}
-                category={item.category}
-                source={item.source}
-                sentiment={item.sentiment}
-                image={item.image}
-              />
+              <Link href={`/article/${item.id}`} key={item.id}>
+                <Card
+                  id={item.id}
+                  title={item.title}
+                  time={item.time}
+                  category={item.category}
+                  source={item.source}
+                  sentiment={item.sentiment}
+                  image={item.image}
+                />
+              </Link>
             ))}
           </div>
         </main>
