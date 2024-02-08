6e7071685ed8300efb6695d2a9d0d79b50e61c8a
rickyaditb <rickyaditb@gmail.com>
2024-02-08 16:16:09 +0700

    fix: picture placement in mobile viewport

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -15,7 +15,7 @@ export default async function page({ params }: { params: NewsItem }) {
         <Sidebar />
         <main className="col-span-6 lg:col-span-5 lg:pl-5">
           <div className="grid grid-cols-12 gap-5">
-            <div className="col-span-12 md:col-span-7">
+            <div className="col-span-12 md:col-span-7 order-2 md:order-1">
               <div className="effect p-7 text-secondary">
                 <h1 className="font-bold text-2xl">{news.title}</h1>
                 <div className="flex gap-8">
@@ -41,7 +41,7 @@ export default async function page({ params }: { params: NewsItem }) {
                 </div>
               </div>
             </div>
-            <div className="col-span-12 md:col-span-5">
+            <div className="col-span-12 md:col-span-5 order-1 md:order-2">
               <div className="effect p-2 rounded">
                 <img src={news.image} alt="" className="rounded" />
               </div>
