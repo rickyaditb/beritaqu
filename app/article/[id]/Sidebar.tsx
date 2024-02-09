cafc500470d8efa04c03cee2060f2b960a61e787
rickyaditb <rickyaditb@gmail.com>
2024-02-09 21:15:46 +0700

    feat: replace next/link in favor of next/router

--- app/article/[id]/Sidebar.tsx
+++ app/article/[id]/Sidebar.tsx
@@ -1,16 +1,18 @@
-import Link from "next/link";
+"use client"
+
+import { useRouter } from 'next/navigation'
 import { FaChevronLeft, FaGlobe, FaMedkit, FaMeh, FaMoneyBill, FaRocket, FaSadTear, FaSmileBeam, FaTags, FaVolleyballBall } from "react-icons/fa";
 
-
 export default function Sidebar({ source, category, sentiment }: { source: string, category: string, sentiment: string }) {
+  const router = useRouter()
   const parsedSentiment = parseFloat(sentiment);
   return (
     <aside className="hidden lg:block">
       <div className="flex flex-col gap-3">
-        <Link href={"/"} className="effect p-4 text-xl xl:text-2xl text-secondary font-semibold flex items-center justify-center gap-2">
+        <button onClick={() => router.back()} className="effect p-4 text-xl xl:text-2xl text-secondary font-semibold flex items-center justify-center gap-2">
           <FaChevronLeft />
           <span>Kembali</span>
-        </Link>
+        </button>
         <div className="effect p-4 flex items-center gap-3">
           <FaGlobe className="text-4xl bg-primary p-2 rounded text-white shrink-0" />
           <p className="text-secondary text-xl xl:text-2xl font-semibold">{source}</p>
