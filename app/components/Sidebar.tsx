e2f92a1009fe51100291952449263defcfec92e0
rickyaditb <rickyaditb@gmail.com>
2024-02-12 19:13:43 +0700

    fix: category filter functionality

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,26 +1,38 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 import { FaMedkit, FaMeh, FaMoneyBill, FaRocket, FaSadTear, FaSmileBeam, FaVolleyballBall } from "react-icons/fa";
 
 export default function Sidebar() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [activeCategory, setActiveCategory] = useState(searchParams.get('category') || '')
+
+  const handleFilter = (keyword: string) => {
+    setActiveCategory(keyword)
+    router.push(`/?category=${keyword}`)
+  } 
   return (
     <aside className="hidden lg:block">
       <div className="flex flex-col gap-3">
         <div className="effect text-center p-6 font-semibold">
           <p className="text-2xl text-secondary border-b-2 pb-3 mb-6">Kategori</p>
           <div className="gap-4 flex flex-col">
-            <button className="flex items-center gap-2 justify-center w-full text-secondary">
+            <button className={`flex items-center gap-2 justify-center w-full ${activeCategory === 'olahraga' ? 'text-primary' : 'text-secondary'}`} onClick={() => handleFilter('olahraga')}>
               <FaVolleyballBall className="text-3xl shrink-0" />
               <span className="text-2xl">Olahraga</span>
             </button>
-            <button className="flex items-center gap-2 justify-center w-full text-secondary">
+            <button className={`flex items-center gap-2 justify-center w-full ${activeCategory === 'ekonomi' ? 'text-primary' : 'text-secondary'}`} onClick={() => handleFilter('ekonomi')}>
               <FaMoneyBill className="text-3xl shrink-0" />
               <span className="text-2xl">Ekonomi</span>
             </button>
-            <button className="flex items-center gap-2 justify-center w-full text-secondary">
+            <button className={`flex items-center gap-2 justify-center w-full ${activeCategory === 'kesehatan' ? 'text-primary' : 'text-secondary'}`} onClick={() => handleFilter('kesehatan')}>
               <FaMedkit className="text-3xl shrink-0" />
               <span className="text-2xl">Kesehatan</span>
             </button>
-            <button className="flex items-center gap-2 justify-center w-full text-secondary">
+            <button className={`flex items-center gap-2 justify-center w-full ${activeCategory === 'teknologi' ? 'text-primary' : 'text-secondary'}`} onClick={() => handleFilter('teknologi')}>
               <FaRocket className="text-3xl shrink-0" />
               <span className="text-2xl">Teknologi</span>
             </button>
