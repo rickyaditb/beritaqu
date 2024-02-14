3e29e008fad710a025614cc59636e9027abf5045
rickyaditb <rickyaditb@gmail.com>
2024-02-14 14:31:57 +0700

    refactor: join prevPage function and nextPage function

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -10,37 +10,30 @@ export default function Pagination() {
 
   const page = parseInt(searchParams.get('page') || '1');
 
-  const prevPage = () => {
-    let url = `/?page=${page - 1}`;
+  const changePage = (increment: number) => {
+    let url = `/?page=${page + increment}`;
     if (searchParams.get('search')) {
       url += `&search=${searchParams.get('search')}`;
     }
     if (searchParams.get('category')) {
       url += `&category=${searchParams.get('category')}`;
     }
-    router.push(url);
-  };
-
-  const nextPage = () => {
-    let url = `/?page=${page + 1}`;
-    if (searchParams.get('search')) {
-      url += `&search=${searchParams.get('search')}`;
-    }
-    if (searchParams.get('category')) {
-      url += `&category=${searchParams.get('category')}`;
+    if (searchParams.get('sentiment')) {
+      url += `&sentiment=${searchParams.get('sentiment')}`;
     }
     router.push(url);
   };
+  
   return (
     <nav className="mt-6">
       <div className="flex gap-3 justify-center text-primary">
-        <button className={`effect text-2xl p-3 ${page === 1 && "opacity-40"}`} onClick={prevPage} disabled={page === 1}>
+        <button className={`effect text-2xl p-3 ${page === 1 && "opacity-40"}`} onClick={() => changePage(-1)} disabled={page === 1}>
           <FaChevronLeft />
         </button>
         <div className="effect text-2xl py-3 px-5 font-bold">
           {page}
         </div>
-        <button className="effect text-2xl p-3" onClick={nextPage}>
+        <button className="effect text-2xl p-3" onClick={() => changePage(1)}>
           <FaChevronRight />
         </button>
       </div>
