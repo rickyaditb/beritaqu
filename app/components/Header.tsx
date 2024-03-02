f63b39534b7b4b48ad3fa85e81cb2e943b99cf80
rickyaditb <rickyaditb@gmail.com>
2024-03-02 14:54:13 +0700

    fix: build error by using Suspense

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,6 +5,7 @@ import { FaBars, FaBookmark, FaChevronLeft, FaCog, FaFilter, FaSearch } from "re
 import { useRouter, usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Drawer from './MobileFilter';
+import { Suspense } from 'react';
 
 export default function Header() {
   const router = useRouter();
@@ -34,7 +35,9 @@ export default function Header() {
         <input
           onChange={(e) => handleSearch(e)}
           type="text" name="" id="" placeholder="Cari Berita Disini" className="text-2xl w-full py-6 ml-5 border-none focus:outline-none rounded-lg" />
-        <Drawer />
+        <Suspense>
+          <Drawer />
+        </Suspense>
       </div>
       <Link href="/bookmark" className="p-6 effect flex items-center justify-center col-span-3 lg:col-span-1 order-2 lg:order-3">
         <FaBookmark className="text-secondary text-4xl shrink-0" />
